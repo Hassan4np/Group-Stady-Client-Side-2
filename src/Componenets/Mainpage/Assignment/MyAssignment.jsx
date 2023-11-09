524cf4be436f6d2ab49effbb2d5fa65e0ecfc0e5
Hassan Ali <hassan4np@gmail.com>
2023-11-09 15:31:39 +0600

    update mysubmit card

--- src/Componenets/Mainpage/Assignment/MyAssignment.jsx
+++ src/Componenets/Mainpage/Assignment/MyAssignment.jsx
@@ -1,58 +1,54 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
 import useAxios from "../../../Hooks/useAxios";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import "react-datepicker/dist/react-datepicker.css";
+import { useEffect } from "react";
 
 const MyAssignment = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     const Axios = useAxios();
+    useEffect(() => {
+        AOS.init({ duration: 2000 });
+    }, [])
     const url = `/submitedata?status=${'complate'}&email=${user.email}`;
 
     const getassignmentdata = async () => {
         const res = await Axios.get(url);
         return res
     }
-    const { isPending,  data } = useQuery({
-        queryKey: ['submiteddata',user.email,"complate"],
+    const { isPending, data } = useQuery({
+        queryKey: ['submiteddata', user.email, "complate"],
         queryFn: getassignmentdata,
 
     })
     if (isPending) {
         return <div className="text-center mt-32">
-        <span className="loading text-center text-green-600 text-2xl loading-dots loading-lg"></span>
-     </div>
-    }
-    
-console.log(data.data)
-    return (
-        <div>
-            <div className='min-h-[350px]'>
-            <div className="overflow-x-auto">
-                <table className="table">
-                    <thead className="text-xl">
-                        <tr>                
-                            <th>Title</th>
-                            <th>Totla Marks</th>
-                            <th> Mark</th>
-                            <th>Student Name</th>
-                            <th>Feedback</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody className="mt-5 text-base text-gray-600 font-bold">
-                       {
-                        data.data.map((infodata)=> <tr key={infodata._id} className="bg-base-200">
-                        <td>{infodata?.title}</td>
-                        <td>{infodata?.marks}</td>
-                        <td>{infodata?.mainmark}</td>
-                        <td>{infodata?.username}</td>
-                        <td>{infodata?.feedback}</td>
-                        <td>{infodata?.status}</td>
-                    </tr>)
-                       }
-                    </tbody>
-                </table>
-            </div>
+            <span className="loading text-center text-green-600 text-2xl loading-dots loading-lg"></span>
         </div>
+    }
+
+    console.log(data.data)
+    return (
+        <div className=" py-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 rounded-md space-x-2 p-10 lg:p-0">
+                {
+                    data?.data?.map((item, idx) =>
+                        <div key={item._id} className="border   rounded-xl bg-gradient-to-r from-green-500 to-blue-500 p-6  space-y-4" data-aos="flip-left">
+                            <div className="flex justify-center "> <h1 className="text 2xl font-bold text-center bg-yellow-400  rounded-full w-[20px]">{idx + 1}</h1></div>
+                            <h1 className="text-2xl font-bold ml-1 ">Title: {item.title}</h1>
+                            <h3 className="text-xl font-bold">Total Marks: {item.marks}</h3>
+                            <h3 className="text-xl font-bold">Mark:{item.mainmark}</h3>
+                            <h5 className="text-lg font-bold"> Student: {item.username}</h5>
+                            <h5 className="text-lg font-bold bg-yellow-300 w-1/2 p-2 rounded-md">Status: {item.status}</h5>
+                            <p className="text-lg font-bold">Notes:</p>
+                            <p className="min-h-[150px] w-[200] text-lg font-bold ">
+                                <span className=" font-normal">{item.feedback}</span>
+                            </p>
+                        </div>)
+                }
+            </div>
         </div>
     );
 };
